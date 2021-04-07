62acd2ef26104b4048ccb6e76edd6567d1fb0bec
Miles Olson <mpolson64@gmail.com>
2021-04-07 10:44:07 -0500

    forms

--- src/components/QueryBuilder.tsx
+++ src/components/QueryBuilder.tsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { Form, Input, Button, Select } from "antd";
+
+const { Option } = Select;
+
+interface QueryBuilderFormValues {
+  username: string;
+  since: number;
+}
 
 const QueryBuilder = () => {
-  return <p>Query Builder</p>;
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+
+  const [form] = Form.useForm();
+
+  const now = new Date();
+  const yesterday = new Date().setDate(now.getDate() - 1);
+  const lastWeek = new Date().setDate(now.getDate() - 7);
+  const lastMonth = new Date().setDate(now.getMonth() - 1);
+
+  const onFinish = (values: QueryBuilderFormValues) => {
+    console.log(values);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  return (
+    <Form {...layout} form={form} name="query-builder" onFinish={onFinish}>
+      <Form.Item
+        name="username"
+        label="Lichess Username"
+        rules={[{ required: true }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item name="since" label="Games from" rules={[{ required: true }]}>
+        <Select
+          placeholder="Load games from past day, week, month..."
+          allowClear
+        >
+          <Option value={yesterday}>Day</Option>
+          <Option value={lastWeek}>Week</Option>
+          <Option value={lastMonth}>Month</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Load Games
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default QueryBuilder;
