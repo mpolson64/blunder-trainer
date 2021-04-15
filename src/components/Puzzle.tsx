c4f75d7b65cd5bd80d3596712f2a03c221022fdd
Miles Olson <mpolson64@gmail.com>
2021-04-15 13:46:30 -0500

    Puzzles

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Chessground from "react-chessground";
-import { Dests, Key } from "chessground/types";
+import { Key } from "chessground/types";
+import { CheckCircleTwoTone, CheckOutlined } from "@ant-design/icons";
 
 import "react-chessground/dist/styles/chessground.css";
 import { PuzzleInfo } from "../types/puzzle-types";
@@ -12,35 +13,118 @@ interface Props {
   info: PuzzleInfo;
 }
 
-const getDests = (fen: string, best: string): Dests => {
-  const chess = new Chess(fen);
-
-  const correct = chess
-    .moves({ verbose: true })
-    .filter((move) => move.san === best)[0];
-
-  return new Map<Key, Key[]>([[correct.from, [correct.to]]]);
-};
-
 const Puzzle = (props: Props) => {
+  console.log(props.info.variation);
   const whiteToMove = props.info.halfmoveNumber % 2 === 1;
+
+  const [completed, setCompleted] = useState(false);
+  const [chess, _setChess] = useState(new Chess(props.info.fen));
+  const [fen, setFen] = useState(props.info.fen);
+  const [halfmove, setHalfmove] = useState(props.info.halfmoveNumber);
+
+  const onMove = (from: Key, to: Key) => {
+    const correct = chess
+      .moves({ verbose: true })
+      .filter(
+        (move) =>
+          move.san ===
+          props.info.variation[halfmove - props.info.halfmoveNumber]
+      )[0];
+
+    chess.move({ to: to as ChessJS.Square, from: from as ChessJS.Square });
+    setFen(chess.fen());
+
+    if (to === correct.to && from === correct.from) {
+      if (
+        halfmove + 1 - props.info.halfmoveNumber ===
+        props.info.variation.length
+      ) {
+        setCompleted(true);
+        return;
+      }
+
+      // Move opponent
+      chess.move(
+        props.info.variation[halfmove + 1 - props.info.halfmoveNumber]
+      );
+      setFen(chess.fen());
+      setHalfmove(halfmove + 2);
+    } else {
+      chess.undo();
+      setFen(chess.fen());
+    }
+  };
+
+  const getMoveable = () => {
+    if (completed) {
+      return {
+        dests: new Map(),
+      };
+    }
+
+    const dests = new Map();
+    chess.SQUARES.forEach((s) => {
+      const ms = chess.moves({ square: s, verbose: true });
+      if (ms.length)
+        dests.set(
+          s,
+          ms.map((m) => m.to)
+        );
+    });
+
+    return {
+      dests,
+      free: false,
+      color: whiteToMove ? "white" : "black",
+      showDests: true,
+    };
+  };
+
   return (
     <>
       <h1>{whiteToMove ? "White" : "Black"} to move</h1>
-
-      <Chessground
-        fen={props.info.fen}
-        turnColor={whiteToMove ? "white" : "black"}
-        movable={{
-          color: whiteToMove ? "white" : "black",
-          free: false,
-          dests: getDests(props.info.fen, props.info.variation[0]),
-          showDests: true,
-        }}
-        premovable={{
-          enabled: false
-        }}
-      />
+      <div>
+        <Chessground
+          fen={fen}
+          onMove={onMove}
+          turnColor={chess.turn() === "w" ? "white" : "black"}
+          movable={getMoveable()}
+          premovable={{
+            enabled: false,
+          }}
+          highlight={{
+            lastMove: false,
+            check: true,
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: "100%",
+            width: "100%",
+            opacity: completed ? 1 : 0,
+            visibility: completed ? "visible" : "hidden",
+            transition: ".3s ease",
+            zIndex: 999,
+          }}
+        >
+          <CheckCircleTwoTone
+            style={{
+              fontSize: "196px",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              textAlign: "center",
+            }}
+            twoToneColor="#52c41a"
+          />
+        </div>
+      </div>
     </>
   );
 };
